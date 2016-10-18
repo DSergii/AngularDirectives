8f75be5bdc552dcbae067a5c104b06a761c2d425
Sergii <drozdsa@gmail.com>
2016-10-18 11:00:43 +0300

    Update image-upload.js

--- image-upload.js
+++ image-upload.js
@@ -10,16 +10,16 @@
 			template: '<input type="file" id="image" >',
 			link: function(scope, element){
 
-				var mimeImgType = ['png', 'jpg', 'jpeg', 'tif', 'bmp', 'gif'];
+				let mimeImgType = ['png', 'jpg', 'jpeg', 'tif', 'bmp', 'gif'];
 
 				element.find('input').bind('change', function(e) {
 
-					var file = e.target.files[0];
-					var type = file.name.substr(file.name.indexOf('.') + 1 );
+					let file = e.target.files[0];
+					let type = file.name.substr(file.name.indexOf('.') + 1 );
 
 					if( mimeImgType.indexOf(type) > -1 ) {
 						
-						var reader = new window.FileReader();
+						let reader = new window.FileReader();
 						reader.readAsDataURL(file);
 						reader.onloadend = function() {
 
