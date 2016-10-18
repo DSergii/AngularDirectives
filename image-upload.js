5256f4fbce577964d00a2b37e45479f7a34edeb4
DSergii <drozdsa@gmail.com>
2016-10-18 10:42:55 +0300

    add js files

---
+++ image-upload.js
@@ -0,0 +1,41 @@
+
+module.exports = angular.module('intercom.directives.imageUpload', [])
+
+	.directive('imageUpload', function () {
+		'use strict';
+
+		return {
+			restrict: 'E',
+			scope: {
+				getImg: '='
+			},
+			template: '<input type="file" id="image" >',
+			link: function(scope, element){
+
+				var mimeImgType = ['png', 'jpg', 'jpeg', 'tif', 'bmp', 'gif'];
+
+				element.find('input').bind('change', function(e) {
+
+					var file = e.target.files[0];
+					var type = file.name.substr(file.name.indexOf('.') + 1 );
+
+					if( mimeImgType.indexOf(type) > -1 ) {
+						
+						var reader = new window.FileReader();
+						reader.readAsDataURL(file);
+						reader.onloadend = function() {
+
+							scope.getImg = '<img  src="'+reader.result+'" alt="image" >';
+
+						}
+
+					} else {
+
+						alert('Sorry, this file type not supported!!');
+
+					}
+
+				});
+			}
+		}
+	});
---
+++ leaflet-map.js
@@ -0,0 +1,50 @@
+module.exports = angular.module('intercom.directives.leafletMap', [])
+
+	.directive('leafletMap', function () {
+		'use strict';
+
+		return {
+
+			restrict: 'E',
+			replace: true,
+			template: '<div></div>',
+			scope: {
+				user: '=',
+				zoom: '='
+			},
+
+			link: function(scope, element){
+
+				let userCoords;
+
+				let userWatch = scope.$watch('user', function(newVal) {
+					if(newVal) {
+						userCoords = [newVal.latitude, newVal.longitude];
+						initMap(userCoords, scope.zoom);
+						userWatch();
+					}
+				});
+				
+				function initMap(coords, zoom) {
+
+					let map = L.map(element[0], {
+						scrollWheelZoom: false,
+						dragging: false,
+						center: coords,
+						zoom: zoom,
+						zoomControl: false
+					});
+
+					L.tileLayer('https://cartocdn_{s}.global.ssl.fastly.net/base-flatblue/{z}/{x}/{y}.png', {
+
+					}).addTo(map);
+
+					let myIcon = L.divIcon({className: 'marker-icon'});
+
+					L.marker(coords, {icon: myIcon}).addTo(map);
+				}
+				
+			}
+		}
+		
+	});
